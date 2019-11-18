255d51e7097001e272dae2031f8b194f2a65685a
Mingwei He <hemingwei2017@gmail.com>
2019-11-17 20:03:40 -0800

    data types and data structures

---
+++ _00_data_types.R
@@ -0,0 +1,23 @@
+# 1. Logical
+v <- TRUE 
+print(class(v))
+
+# 2. Numeric
+v <- 23.5
+print(class(v))
+
+# 3. Integer
+v <- 2L
+print(class(v))
+
+# 4. Complex
+v <- 2+5i
+print(class(v))
+
+# 5. Character
+v <- "TRUE"
+print(class(v))
+
+# 6. Raw
+v <- charToRaw("Hello")
+print(class(v))
